5ed4193d728aacaca3912df21ad57af00c872a38
Camila-Arredondo <camiarredondo@MacBook-Air-de-Camila.local>
2022-07-26 15:47:01 -0400

    Âdetalles

--- src/Components/NavBar/NavBarMenuUp.jsx
+++ src/Components/NavBar/NavBarMenuUp.jsx
@@ -5,7 +5,16 @@ import { Container, Nav, Navbar } from "rsuite";
 import 'App.css'
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { BtnLogout } from "Components/btnLogout";
-
+const tiposComida = [
+  {
+    value: "desayuno",
+    label: "Desayuno"
+  },
+  {
+    value: "almuerzo",
+    label: "Almuerzo"
+  }
+]
 const CustomNav = ({ active, onSelect, ...props }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
@@ -13,18 +22,27 @@ const CustomNav = ({ active, onSelect, ...props }) => {
     <Navbar className="NavBarTable">
 
       <Navbar.Brand onClick={() => {
+        if (searchParams.get("tipo") === "mesero") {
           navigate("/TableNumber")
-        }}>
+        }
+
+      }}>
+        {/* {
+          true ? <div>verdadero</div> : <div>falso</div>
+        } */}
         <img className="" style={{
           width: "50px",
           marginTop: "-10px"
         }} src="/image/SoloLogo.png" alt="" />
+
+
       </Navbar.Brand>
 
 
 
 
       <Nav {...props} activeKey={active} onSelect={onSelect} >
+        
         <Nav.Item eventKey="desayuno">Desayuno</Nav.Item>
         <Nav.Item eventKey="almuerzo">Almuerzo</Nav.Item>
 
@@ -34,8 +52,8 @@ const CustomNav = ({ active, onSelect, ...props }) => {
         <label>Mesa  {searchParams.get("mesa")}</label>
       </Nav>
       <Nav pullRight>
-               <BtnLogout />
-            </Nav>
+        <BtnLogout />
+      </Nav>
 
     </Navbar>
   );
