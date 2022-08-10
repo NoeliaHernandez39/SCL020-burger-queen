663d2a50dd141018c7c021cb41f3742ab16da51d
Camila-Arredondo <camiarredondo@MacBook-Air-de-Camila.local>
2022-08-10 01:59:04 -0400

    vista final pedido

--- src/pages/WaiterOrderCartPage.jsx
+++ src/pages/WaiterOrderCartPage.jsx
@@ -1,5 +1,6 @@
 import { NavOrderUnder } from "Components/NavBar/NavBarOrderCartUnder";
 import { NavOrderUp } from "Components/NavBar/NavBarOrderCartUp";
+import { ProductoItem } from "Components/producto";
 import React, { useContext, useEffect } from "react";
 import { Content, Footer, Header } from "rsuite";
 import PaginaContext from "utils/pagina.context";
@@ -19,10 +20,12 @@ export function WaiterOrderCartPage() {
                 <Content>
                {
                 carritoCompra.map((x, index)=>{
-                    return <div key={index}>
-                    <label>{x.nombre}</label>
-                    <br></br>
-                    </div>
+                    return (
+                        <div key={index}>{/* cuando tenga map y me retorne un html debe ir un key en el primer elemento*/}
+                            <ProductoItem
+                                dataItem={x} dondeEstoy={"carrito"}                            />
+                        </div>
+                    )
                 })
                }
                 </Content>
